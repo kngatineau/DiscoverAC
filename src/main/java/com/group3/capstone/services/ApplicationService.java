3fb390ff3a2ec3b665bd0dcb42ed3c9566401a0c
aladah <aladah.moore@gmail.com>
2022-02-24 10:36:02 -0500

    Committing branch for Aladah Feb 24 work

--- src/main/java/com/group3/capstone/services/ApplicationService.java
+++ src/main/java/com/group3/capstone/services/ApplicationService.java
@@ -1,5 +1,24 @@
 package com.group3.capstone.services;
 
-public interface ApplicationService {
+import java.util.List;
+import java.util.Map;
 
+import com.group3.capstone.beans.Bulletin;
+import com.group3.capstone.beans.Post;
+
+public interface ApplicationService {
+	
+	//view entire AC bulletin, integer stands for our ID 
+	public Map<Integer, Bulletin> readACBulletin();
+	
+	//view child AC bulletin
+	public List<Post> readBulletin(int bulletinID);
+	
+	//for individual Posts
+	public Post readPost(String id);
+	
+	public void writePost(Post post);
+	
+	public void deletePost(String id);
+	
 }
