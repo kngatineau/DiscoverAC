935d7ccc15f3c05747e2f4a66c08b2b9f8433111
aladah <aladah.moore@gmail.com>
2022-02-28 14:25:38 -0500

    Login page is the initial page

--- src/main/java/com/group3/capstone/servlets/RegisterServlet.java
+++ src/main/java/com/group3/capstone/servlets/RegisterServlet.java
@@ -37,7 +37,7 @@ public class RegisterServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		//String page = getHTMLString(request.getServletContext().getRealPath("/register.html"));
-		String page = getHTMLString(request.getServletContext().getRealPath("/login.html"));
+		String page = getHTMLString(request.getServletContext().getRealPath("/index.html"));
 		response.getWriter().write(page);
 	}
 
@@ -88,7 +88,7 @@ public class RegisterServlet extends HttpServlet {
 		}
 		else {
 			System.out.println("Access Denied");
-			String page = getHTMLString(request.getServletContext().getRealPath("/login.html"));
+			String page = getHTMLString(request.getServletContext().getRealPath("/index.html"));
 			//ideally would like alert to user to try again 
 			PrintWriter writer = response.getWriter();
 			writer.write(page);
