a98aaa45254455a9c893c3412a3c5a48c153ac82
Kaitlyn Gatineau <gati0010@algonquinlive.com>
2022-03-25 21:16:36 -0600

    Added comments on ProfileServlet/touched up CSS

--- src/main/java/com/group3/capstone/servlets/ProfileServlet.java
+++ src/main/java/com/group3/capstone/servlets/ProfileServlet.java
@@ -12,7 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.group3.capstone.dao.ApplicationDao;
 import com.group3.capstone.dao.ApplicationDaoProxy;
 import com.group3.capstone.services.ApplicationService;
 import com.group3.capstone.user.User;
@@ -29,7 +28,7 @@ public class ProfileServlet extends HttpServlet {
 	User user;
 	UUID sessionId = null;
 	String message = "";
-       
+	String SID;
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -43,27 +42,25 @@ public class ProfileServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		// if the Submit button is pressed under the 'Edit' form
 		if (request.getParameter("uname") != null) {
 			updateUserInfo(request, response);
 			session = dao.getSession(sessionId);
 			response.sendRedirect("profile?session="+ session.getSessionId().toString());
 		}
-		
+		// sets sessionId to equal the session parameter if not null
 		if (request.getParameter("session") != null) {
 			sessionId = UUID.fromString(request.getParameter("session"));
-			// SID = sessionId.toString();
+			SID = sessionId.toString();
 		}
 		String page = getHTMLString(request.getServletContext().getRealPath("/profile.html"));
 		page = MessageFormat.format(page, message);
 		response.getWriter().write(page);
 		response.getWriter().write(printUserInfo(request, response, sessionId));
-		System.out.println("SESSION ID: " + sessionId);	
 	
 	}
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		User user = dao.getSession(sessionId).getUser();
-		String page = getHTMLString(request.getServletContext().getRealPath("/profile.html"));
 		String userName = user.getUserName();
 		String currentPassword = request.getParameter("currentPass");
 		String newPassword = request.getParameter("newPass");
@@ -71,20 +68,22 @@ public class ProfileServlet extends HttpServlet {
 		
 		boolean permissionGranted = dao.verifyUserNamePassword(userName, currentPassword);
 		
+		// if the 'current password' entry matches the password on file
 		if (permissionGranted) {
 			session = dao.getSession(sessionId);
+			// if the two 'new password' entries match
 			if (newPassword.equals(confirmPassword)) {
 			dao.updateUserPassword(user, newPassword);
 			message = "<h3 style=\"margin:auto; text-align:center;color:green\">"
 					+ "Password changed successfully.</h3>";
 			response.sendRedirect("profile?session="+ session.getSessionId().toString());
 			}
-			else {
+			else { // error for non-matching 'new password' entries
 				message = "<h3 style=\"margin:auto; text-align:center;color:red\">"
 						+ "Passwords do not match.</h3>";
 				response.sendRedirect("profile?session="+ session.getSessionId().toString() + "&name=changePass");
 			}
-		} else {
+		} else { // error for incorrect 'current password'
 			message = "<h3 style=\"margin:auto; text-align:center;color:red\">"
 					+ "Incorrect password.</h3>";
 			response.sendRedirect("profile?session="+ session.getSessionId().toString() + "&name=changePass");
@@ -109,11 +108,12 @@ public class ProfileServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 
-	protected String printUserInfo(HttpServletRequest request, HttpServletResponse response, UUID sessionID) {
+	protected String printUserInfo(HttpServletRequest request, HttpServletResponse response, UUID sessionId) {
 		session = dao.getSession(sessionId);
 		User user = dao.getSession(sessionId).getUser();
 		String userInfo;
 		message = "";
+		// prints if the 'Edit' link is clicked
 		if (request.getParameter("name") != null && request.getParameter("name").equals("edit")) {
 			userInfo =  "<div style='margin: auto; text-align: center;'><form method=\"get\"><table style='margin: auto; text-align: center;'>"
 					+ "<tr><td><label for=\"fname\">First Name: </label></td><td><input type=\"text\" id=\"fname\" name=\"fname\" value='" 
@@ -123,12 +123,12 @@ public class ProfileServlet extends HttpServlet {
 					+ "<tr><td><label for=\"uname\">Username: </label></td><td><input type=\"text\" id=\"uname\" name=\"uname\" value='" 
 					+ user.getUserName() + "'></td></tr><td></table>" 
 					+ user.getEmail() 
-					+ "</br><a href='profile?session=" + sessionID + "&name=changePass'>Change Password</a></br>"
+					+ "</br><a href='profile?session=" + SID + "&name=changePass'>Change Password</a></br>"
 					+ "</br><input type=\"submit\" value=\"Submit\">"
 					+ "</form>"
-					+ "<form method=\"get\" action='profile?session=" + sessionID + "'>"
+					+ "<form action='profile?session=" + SID + "'>"
 					+ "<input type=\"submit\" value=\"Back to Profile\"></form></div>\r\n"; 
-
+		// prints if the 'change password' link is clicked
 		} else if (request.getParameter("name") != null && request.getParameter("name").equals("changePass")) { 
 			userInfo =  "<div style='margin: auto; text-align: center;'><form method=\"post\">"
 					+ "<table style='margin: auto; text-align: center;'>"
@@ -137,17 +137,17 @@ public class ProfileServlet extends HttpServlet {
 					+ "<tr><td><label for=\"confirmPass\">Confirm Password: </label></td><td><input type=\"text\" id=\"confirmPass\" name=\"confirmPass\"><td></tr>"
 					+ "</table></br><input type=\"submit\" value=\"Submit\">"
 					+ "</form>"
-					+ "<form method=\"get\" action='profile?session=" + sessionID + "'>"
+					+ "<form action='profile?session=" + SID + "'>"
 					+ "<input type=\"submit\" value=\"Back to Profile\"></form></div>\r\n"; 
+			
+			// prints profile information otherwise
 		} else {
-
-		
 		userInfo = "<div style='margin: auto; width: 300px; text-align: center'>"
 				+ user.getFirstName() + " " + user.getLastName()
 				+ "</br>" +user.getUserName()
 				+ "</br>" + user.getEmail()
-				+ "</br><a href='profile?session=" + sessionID + "&name=edit'>Edit</a>"		
-				+ "<form method=\"get\" action='dashboard?session=" + sessionID + "'>"
+				+ "</br><a href='profile?session=" + SID + "&name=edit'>Edit</a>"		
+				+ "<form method='get' action=\"dashboard?session=" + SID + "\">"
 						+ "</br><input type=\"submit\" value=\"Back to Bulletin Board\"></form>\r\n"
 				+ "</div>";
 		}
@@ -155,8 +155,8 @@ public class ProfileServlet extends HttpServlet {
 		
 	} 
 	protected void updateUserInfo(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		// method called when 'Submit' is pressed using the 'Edit' form
 		User user = dao.getSession(sessionId).getUser();
-		// ApplicationDao dao = new ApplicationDao();
 		user.setUserName(request.getParameter("uname"));
 		user.setFirstName(request.getParameter("fname"));
 		user.setLastName(request.getParameter("lname"));
