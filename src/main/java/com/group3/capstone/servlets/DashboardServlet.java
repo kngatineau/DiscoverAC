23197f2e38bef5fef5a154237f591fc598705729
Hyosung Jung <hyosung.j@posteo.org>
2022-04-08 15:06:57 -0400

    trim unused lines

--- src/main/java/com/group3/capstone/servlets/DashboardServlet.java
+++ src/main/java/com/group3/capstone/servlets/DashboardServlet.java
@@ -1,7 +1,6 @@
 package com.group3.capstone.servlets;
 
 import java.io.IOException;
-import java.io.Writer;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -31,9 +30,6 @@ public class DashboardServlet extends HttpServlet {
 	User user = null;
 	String page = null;
 	ApplicationService appDB = new ApplicationDaoProxy();
-	//ApplicationDao appDB = new ApplicationDao();
-	
-	
 	
 	//Initialize bulletinId with Algonquin College bulletin ID.
 	UUID bulletinId = UUID.fromString("930cc92d-8a1f-4ba5-90b6-4373d90d6e22");
@@ -50,12 +46,7 @@ public class DashboardServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Dashboard servlet initiated");
-//		//Temporary Face
-//		page = getHTMLString(request.getServletContext().getRealPath("/dashboard.jsp"));
-//		response.getWriter().write(page);
-		
-		//User Entry Logic
-		Writer writer = response.getWriter();
+
 		
 		// Check if sessionId is in an appropriate format.
 		Boolean sessionIdWrongFormat = false;
@@ -114,87 +105,14 @@ public class DashboardServlet extends HttpServlet {
 				request.setAttribute("bulletin", bulletin);
 				request.setAttribute("posts", posts);
 				request.setAttribute("authors", authors);
-				
-				
-				
+								
 				request.getRequestDispatcher("/dashboard.jsp").forward(request, response);
-				
-	
-				// Below now handled by JSP
-//				page = getHTMLString(request.getServletContext().getRealPath("/dashboard.html"));
-//				page = MessageFormat.format(page, user.getFirstName(), user.getLastName());
-//				
-//				try {
-//					// Write bulletin posts to page if posts exist.
-//					page += populatePosts(appDB.getBulletinPosts(bulletinId));
-//
-//				} catch (SQLException e) {
-//					e.printStackTrace();
-//				}
-//			
-//				writer.write(page);
 				
 			}
 
 		}
 		
 	}
-	
-//    public String populatePosts(List<Post> posts) throws SQLException{
-//    	
-//    	String htmlResults = " <form method=\"get\" style='margin: auto; text-align: center;'>\r\n"
-//						+ "    <button type=\"submit\" name=\"profile\">My Profile</button>\r\n"
-//						+ " </form>" 		
-//    			+ "<h4 style='text-align:center; font-size: 25px;'>"
-//    			+ appDB.getBulletin(bulletinId).getBulletinName() +" Bulletin Board:</h4>"
-//    			+ "<table style='border-collapse: collapse;'>"
-//        		+ "        <thead>\n"
-//        		+ "            <tr>\n"
-//        		+ "                <th>Title</th>\n"
-//        		+ "                <th>Description</th>\n"
-//        		+ "                <th>URL</th>\n"
-//        		+ "                <th>Post Date</th>\n"
-//        		+ "                <th>Author</th>\n"
-//        		+ "            </tr>\n"
-//        		+ "        </thead>\n";
-//    	User user = null;
-//    	
-//    	
-//    	for(Post post: posts) {
-//    		user = appDB.getUser(post.getAuthorId());
-//    		htmlResults += "<tr style='height:100px; background-color:#ccedc5; "
-//    				+ "border-bottom: 10px solid white;'>\n"
-//             		+ "			<td style='padding-right:20px;''>"+ post.getTitle()+"</td>\n"
-//            		+ "			<td style='width:300px; padding-right:20px;'>"
-//             		+ post.getDescription()+"</td>\n"
-//            		+ "         <td style='padding-right:20px;'>"
-//            		+ "  			<a href=\""+ post.getUrl()+"\">"+post.getUrl()+"</a>"
-//             		+ "			</td>\n"
-//             		+ "         <td style='padding-right:20px;'>"+ post.getPostDate().toString()+"</td>\n"
-//                    + "         <td style='padding-right:20px;'>"+ user.getUserName()+"</td>\n"
-//            		+ "</tr>";
-//    	}
-//    	
-//        // Finally, add the closing html:
-//        htmlResults += "        </tbody></table>";
-//    	
-//    	return htmlResults;
-//    	
-//    }
-//
-//	public String getHTMLString(String filePath) throws IOException {
-//		BufferedReader reader = new BufferedReader(new FileReader(filePath));
-//		String line = "";
-//		StringBuffer buffer = new StringBuffer();
-//		while ((line = reader.readLine()) != null) {
-//			buffer.append(line);
-//		}
-//
-//		reader.close();
-//		String page = buffer.toString();
-//
-//		return page;
-//	}
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
