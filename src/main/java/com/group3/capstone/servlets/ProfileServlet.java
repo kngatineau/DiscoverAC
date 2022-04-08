5192af5560197afddaca6fa874004f3b3642877a
Hyosung Jung <hyosung.j@posteo.org>
2022-04-08 17:02:16 -0400

    Clean up unused import

--- src/main/java/com/group3/capstone/servlets/ProfileServlet.java
+++ src/main/java/com/group3/capstone/servlets/ProfileServlet.java
@@ -3,7 +3,6 @@ package com.group3.capstone.servlets;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.MessageFormat;
 import java.util.UUID;
 
 import javax.servlet.ServletException;
